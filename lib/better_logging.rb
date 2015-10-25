3b3385e0c48b7f5d815df5b21d0cbc2896249ab5
Scott Nelson <Scott.D.Nelson@dartmouth.edu>
2015-10-25 11:29:01 -0700

    Take params out of path info. Add Filter params extension.

--- lib/better_logging.rb
+++ lib/better_logging.rb
@@ -1,5 +1,6 @@
 require "better_logging/version"
 require "better_logging/rails_extensions/log_subscriber_extension.rb"
+require "better_logging/rails_extensions/filter_parameters_extension.rb"
 
 module BetterLogging
 end
---
+++ lib/better_logging/rails_extensions/filter_parameters_extension.rb
@@ -0,0 +1,14 @@
+require 'action_dispatch/http/parameter_filter'
+
+# module ActionDispatch::Http::FilterParameters
+#
+# end
+module ActionDispatch
+  module Http
+    module FilterParameters
+      def filtered_path
+        @filtered_path ||= query_string.empty? ? path : "#{path}"
+      end
+    end
+  end
+end
