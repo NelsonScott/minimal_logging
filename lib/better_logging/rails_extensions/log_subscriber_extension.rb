03724a126b091af143e2f61e091f7dc11d12f599
Scott Nelson <Scott.D.Nelson@dartmouth.edu>
2015-10-25 11:13:52 -0700

    Update 'Completed' message. Colorize status code

--- lib/better_logging/rails_extensions/log_subscriber_extension.rb
+++ lib/better_logging/rails_extensions/log_subscriber_extension.rb
@@ -19,4 +19,34 @@ class ActionController::LogSubscriber
       ap params
     end
   end
+
+  def process_action(event)
+    return unless logger.info?
+
+    payload   = event.payload
+    additions = ActionController::Base.log_process_action(payload)
+
+    status = payload[:status]
+    if status.nil? && payload[:exception].present?
+      exception_class_name = payload[:exception].first
+      status = ActionDispatch::ExceptionWrapper.status_code_for_exception(exception_class_name)
+    end
+    message = format_message(status)
+
+    info(message)
+  end
+
+  def format_message(status)
+    if status < 300
+      status.to_s.colorize(:green)
+    elsif status < 400
+      status.to_s.colorize(:yellow)
+    elsif status < 500
+      status.to_s.colorize(:red)
+    elsif status < 600
+      status.to_s.colorize(:light_red)
+    else
+      status
+    end
+  end
 end
