1ad297b7db1369481877dfee7c0b56d186624878
Scott Nelson <Scott.D.Nelson@dartmouth.edu>
2015-11-08 14:01:50 -0800

    Log level set early in rails stack.  Call rails directly to chane it.

--- lib/minimal_logging.rb
+++ lib/minimal_logging.rb
@@ -2,7 +2,7 @@ require "minimal_logging/version"
 
 module MinimalLogging
   def self.setup(app)
-    app.config.log_level = :info if app.config.minimal_logging.change_log_level
+    Rails.logger.level = 1 if app.config.minimal_logging.change_log_level
     require "minimal_logging/rails_extensions/action_view_log_subscriber_extension.rb"
     require "minimal_logging/rails_extensions/filter_parameters_extension.rb"
     require "minimal_logging/rails_extensions/log_subscriber_extension.rb"
